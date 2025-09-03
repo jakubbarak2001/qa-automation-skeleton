a268d573a61fe1198b4a9ca7d3bbd132bd0a9bbd
Jakub Barák <youremail@example.com>
2025-09-03 17:59:14 +0200

    CPG-9: Add invalid login test (wrong password case)

--- tests/test_login.py
+++ tests/test_login.py
@@ -8,3 +8,12 @@ def test_login(page, base_url):
     page.locator('[data-test="login-button"]').click()
     page.wait_for_selector('[data-test="inventory-list"]', state='visible')
     assert page.locator('[data-test="inventory-list"]').is_visible()
+
+
+def test_login_wrong(page, base_url):
+    page.goto(base_url)
+    page.locator('[data-test="username"]').fill('standard_user')
+    page.locator('[data-test="password"]').fill('wrong_password')
+    page.locator('[data-test="login-button"]').click()
+    page.wait_for_selector('[data-test="error"]', state='visible')
+    assert page.locator('[data-test="error"]').is_visible()
